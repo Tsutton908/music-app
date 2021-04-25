8b6af503cbbd119936f28a40319077b4e05a1b97
William Sutton <tsutton908@gmail.com>
2021-04-24 21:01:35 -0400

    removed functions and variables no longer being used. More comments added for ease of use and understanding.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 
 import Login from './components/Login';
@@ -14,8 +14,9 @@ const spotify = new SpotifyWebApi();
 
 function App() {
 
-  const [{ user, token, topTracks }, dispatch] = useDataLayerValue();
+  const [{ token }, dispatch] = useDataLayerValue();
 
+  //constant set at the beginning of loaded page to update the background color to be randomly selected
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
   useEffect(() => {
@@ -26,6 +27,7 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
+      //sets the token as constant state variable in the redux reducer
       dispatch({
         type: 'SET_TOKEN',
         token: _token
@@ -35,7 +37,7 @@ function App() {
 
       //gets the user associated with the logged in token
       spotify.getMe().then(user => {
-        
+        //pushes the user's account id to the redux reducer's 'user' variable
         dispatch({
           type: 'SET_USER',
           user: user,
@@ -43,58 +45,57 @@ function App() {
       })
     }
 
+    //gets the loggged in user's saved playlists 
     spotify.getUserPlaylists().then((playlists) => {
+      //pushes the user's playlist object to the redux reducer's 'playlists' array variable
       dispatch({
         type: 'SET_PLAYLIST',
         playlists: playlists,
       })
     })
 
-    
-    spotify.getPlaylist('60vUcvSmx6gubMzTMlMXW1').then(response => {
-      dispatch({
-        type: 'SET_DISCOVER_WEEKLY',
-        discover_weekly: response,
-      })
-    })
-
+    //gets and returns object for the current logged in user's top 10 listened to artist's
     spotify.getMyTopArtists().then((topArtists) => {
+      //pushes the user's top artist's object to the redux reducer's 'topArtists' array variable
       dispatch({
         type: 'SET_TOP_ARTISTS',
         topArtists: topArtists,
       })
     })
 
+    //get and returns object conatining current user's top 25 listened to tracks
     spotify.getMyTopTracks().then((topTracks) => {
+      //pushes the user's top tracks object to the redux reducer's 'topTracks' array variable
       dispatch({
         type: 'SET_TOP_TRACKS',
         topTracks: topTracks,
       })
     })
 
-    spotify.getMyDevices().then((devices) => {
-      let availableDevices = devices.data;
-      dispatch({
-        type: 'SET_DEVICES',
-        devices: devices,
-      })
-    })
-
+    //gets and returns the user's recently played tracks 
     spotify.getMyRecentlyPlayedTracks().then((initialSong) => {
+      //only the last track is desired, so song is initialized with only the last played track by the user
+      //this is only for the player to be able to play recent track without a track having to be selected
       let song = initialSong.items[0].track.uri.slice(14);
+      //pushes the user's recently played track to the redux reducer's 'song' variable
+      //sets the player's currently playing song.
       dispatch({
         type: 'SET_SONG',
         song: song,
       })
     })
 
+    //gets and returns spotify's suggested new realeases object 
     spotify.getNewReleases().then((newReleases) => {
+      //pushes spotify's new releases to the redux reducer's 'newReleases' variable
       dispatch({
         type: 'SET_NEW_RELEASES',
         newReleases: newReleases,
       })
     })
 
+    //pushes the random color initialized before to the redux reducers 'randomColor' variable 
+    //this is done so that the color can be changed anytime the state is changed and can be accessed through other components
     if (randomColor) {
       dispatch({
         type: 'SET_RANDOM_COLOR',
--- src/DataLayer.js
+++ src/DataLayer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createContext, useContext, useReducer } from 'react';
 export const DataLayerContext = createContext();
 
+//initialize the datalayer using the redux reducer and the intitail states stated in the reducer file
 export const DataLayer = ({ initialState, reducer, children }) => (
     <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
         {children} 
